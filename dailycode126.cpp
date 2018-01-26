92fc52e7da3959e14a4f6a6f6c3c3f5363347990
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-01-26 14:51:51 -0700

    Add files via upload

---
+++ dailycode126.cpp
@@ -0,0 +1,25 @@
+#include <allegro5\allegro.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	al_init();
+	ALLEGRO_DISPLAY *display = NULL;
+
+	display = al_create_display(600, 600);
+
+	srand(time(NULL));
+	while (1) {
+		al_clear_to_color(al_map_rgb(rand(), rand(), rand()));
+		al_flip_display();
+		system("pause");
+
+	}
+
+
+	al_destroy_display(display);
+	return 0;
+
+}
